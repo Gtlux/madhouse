e7b6f7b6bd4d6d64044bd3b7a7f79ec5bcce2c77
Gtlux <35406910+Gtlux@users.noreply.github.com>
2018-01-14 00:26:10 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -67,7 +67,12 @@ Multiple images will automatically shuffle between them
 (Place the files in the backgrounds/images folder)*/
 var l_bgImages = [
 	"608333.jpg",
-	"7f561da1cae936756060693b1f1a1c19.jpg"
+	"7f561da1cae936756060693b1f1a1c19.jpg",
+	"anime-boy-psycho-tongue-anime-10566.jpg",
+	"bf765e6954e795d246adb231e39a2f16-d6an9d3.png",
+	"ynthia-leman-mob-psycho100-gratitude-leman-cynthia-1920-1080.jpg",
+	"maxresdefault.jpg"
+
 ];
 
 /*(Image-background only)
