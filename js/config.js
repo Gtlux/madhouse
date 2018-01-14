3fedfc28f869ef30020608b6b526850a0ed4ed1e
Gtlux <35406910+Gtlux@users.noreply.github.com>
2018-01-14 03:23:37 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -109,10 +109,8 @@ var l_musicDisplay = true;
 Add as many youtube ids or ogg files you want
 (Place the ogg files in the music folder)*/
 var l_musicPlaylist = [
-	{ogg: "lullaby.ogg", name: "Lullaby of Deserted Hell"},
-	{ogg: "falls_in_fall.ogg", name: "秋に舞う椛と滝飛沫"},
-	{youtube: "zA6wRutN_7Q", name: "Suwa-cafeteria"},
-	{youtube: "KtlKbE4qU0c", name: "月の妖鳥、化猫の幻"},
+	{youtube: "sF0QLtk3YH0", name: "Psycho-pass ED 1 - Monster without a name"},
+	{youtube: "yoWCsnoHg", name: "ME!ME!ME! Short Version"},
 ];
 
 /*Random music order?
