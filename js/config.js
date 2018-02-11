4c7948162015a687e6bb00947d8d772814c1c3eb
Gtlux <35406910+Gtlux@users.noreply.github.com>
2018-02-11 20:04:50 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -73,11 +73,9 @@ var l_bgImages = [
 	"ynthia-leman-mob-psycho100-gratitude-leman-cynthia-1920-1080.jpg",
 	"maxresdefault.jpg",
 	"01.jpg",
-	"02.jpg",
 	"03.jpg",
 	"04.jpg",
 	"05.jpg",
-	"06.jpg",
 	"07.jpg",
 	"08.jpg",
 	"09.jpg"
