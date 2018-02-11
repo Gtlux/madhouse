f52868b73a72338805a80e8fabc31434a24853e1
Gtlux <35406910+Gtlux@users.noreply.github.com>
2018-02-11 19:05:27 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -71,7 +71,12 @@ var l_bgImages = [
 	"anime-boy-psycho-tongue-anime-10566.jpg",
 	"bf765e6954e795d246adb231e39a2f16-d6an9d3.png",
 	"ynthia-leman-mob-psycho100-gratitude-leman-cynthia-1920-1080.jpg",
-	"maxresdefault.jpg"
+	"maxresdefault.jpg",
+	"01.jpg",
+	"02.jpg",
+	"03.jpg",
+	"04.jpg",
+	"05.jpg"
 
 ];
 
